3b7473f0072fc5237f9a9840170d42ecbe78be4a
charles <wanchuan21@gmail.com>
2021-08-07 17:59:35 +0800

    new

--- main.js
+++ main.js
@@ -8,6 +8,11 @@ app.get('/server', (request, response) => {
     response.send("HELLO AJAX");
 })
 
+app.post('/server', (request, response) => {
+    // 设置响应
+    response.setHeader('Access-Control-Allow-Origin','*'); // 设置允许跨域
+    response.send("HELLO AJAX POST");
+})
 
 app.listen(8000, ()=> {
     console.log("服务已经启动，8000 端口监听中...");
